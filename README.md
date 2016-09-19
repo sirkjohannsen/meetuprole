ad9ff8319be493a61e6795b71aa964b0b64c3bc1
Sirk Johannsen <sirk.johannsen@simplesurance.de>
2016-09-19 10:43:35 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# meetuprole
-sample role for ansible meetup
+Role Name
+=========
+
+Sample role for ansible meetup
+
+Requirements
+------------
+
+* ruby
+* bundler
+* test-kitchen (gem)
+* kitchen-ansible (gem)
+* kitchen-docker (gem)
+
+Setup
+--------------
+
+checkout repo
+```
+bundle
+```
+
+Usage
+------------
+
+```
+kitchen test
+```
---
+++ test/integration/default/serverspec/main/main_spec.rb
@@ -0,0 +1,16 @@
+# main-spec
+require 'spec_helper'
+
+#describe package('nginx') do
+#  it { should be_installed }
+#end
+#
+#describe service('nginx') do
+#  it { should be_enabled }
+#  it { should be_running }
+#end
+#
+#describe command('curl http://localhost:80') do
+#  its(:exit_status) { should eq 0 }
+#  its(:stdout) { should match /.*nginx.*/ }
+#end
